1402271ac87b5f26c3c9931a8c003c61a5d3d2e9
ccpu <m.doaie@hotmail.com>
2025-08-24 12:25:45 +0100

    chore(lint-staged): add 'css' extension to linting configuration
    * Updated lint-staged configuration to include 'css' files for linting and formatting.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   '*.{ts,tsx}': () => ['pnpm run typecheck'],
-  '*.{ts,tsx,js,jsx,yml,yaml,json}': () => ['pnpm run lint:fix', 'pnpm run format:fix'],
+  '*.{ts,tsx,js,jsx,yml,yaml,json,css}': () => [
+    'pnpm run lint:fix',
+    'pnpm run format:fix',
+  ],
 };
