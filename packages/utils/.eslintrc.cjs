82d42cf99e9e5155ebb09aaf74ecf97cf99a2097
ccpu <m.doaie@hotmail.com>
2025-09-05 10:47:57 +0100

    chore(utils): remove unused ESLint configuration
    * Deleted `.eslintrc.cjs` as it is no longer needed.
    * This helps to clean up the codebase and reduce clutter.

--- packages/utils/.eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-require('../../config/patch');
-
-module.exports = {
-  extends: ['@internal/eslint-config'],
-  parserOptions: { tsconfigRootDir: __dirname },
-  ignorePatterns: ['node_modules', 'dist'],
-};
